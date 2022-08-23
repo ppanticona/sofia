322b87b9b5c25d04db8862be408df898c0167b25
ppanticona <ppanticona@gmail.com>
2022-08-23 09:19:30 -0500

    Initial version of sofia generated by generator-jhipster@7.9.2

---
+++ src/main/java/com/ppanticona/sofia/config/dbmigrations/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * MongoDB database migrations using Mongock.
+ */
+package com.ppanticona.sofia.config.dbmigrations;
---
+++ src/main/java/com/ppanticona/sofia/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.ppanticona.sofia.repository;
+
+import com.ppanticona.sofia.domain.Authority;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends MongoRepository<Authority, String> {}
